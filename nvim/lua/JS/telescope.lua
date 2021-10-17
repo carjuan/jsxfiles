eec23ff064615f5b2d8a35ce90a5cee4c446bbc7
Juanse Carvajal <juanse@Juanses-MBP.hitronhub.home>
2021-10-16 22:06:09 -0500

    refactor(nvim): rename environment var to NVIM_DOTFILES

--- nvim/lua/JS/telescope.lua
+++ nvim/lua/JS/telescope.lua
@@ -4,7 +4,7 @@ local M = {}
 M.search_dotfiles = function()
     require('telescope.builtin').find_files({
         prompt_title = '< VimRC >',
-        cwd = vim.env.DOTFILES,
+        cwd = vim.env.NVIM_DOTFILES,
         hidden = true,
     })
 end
