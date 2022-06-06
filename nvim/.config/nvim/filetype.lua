65ae45915f1ee045a32cf3614e8e441f6fa983b4
Juanse <pcjvan@gmail.com>
2022-06-05 22:37:49 -0500

    feat(nvim): execute same command for running bash, js, lua and csharp files

--- nvim/.config/nvim/filetype.lua
+++ nvim/.config/nvim/filetype.lua
@@ -5,15 +5,54 @@ local _local_csharp_options = {
   shiftwidth = 4,
 }
 
-local set_options = function()
+-- TODO: refactor - too many repeated functions for setting keymaps | noremap is used too many times
+-- check what these callbacks are passed, maybe the file extension?
+-- if callback is passed filetype
+--  I could map configuration in a table for each filetype
+--  and loop over it
+local set_and_run_cs_files = function()
   for key, value in pairs(_local_csharp_options) do
     _global_opts[key] = value
   end
+  vim.keymap.set('n', '<leader>rf', ':w | :!dotnet run<CR>', { noremap = true })
+end
+
+local run_lua_files = function()
+  vim.keymap.set('n', '<leader>rf', ':w | luafile %<CR>', { noremap = true })
+end
+
+local run_bash_files = function()
+  vim.keymap.set('n', '<leader>rf', ':w | :!./%<CR>', { noremap = true })
+end
+
+local run_js_files = function()
+  vim.keymap.set('n', '<leader>rf', ':w | :!node %<CR>', { noremap = true })
 end
 
 vim.api.nvim_create_autocmd(
   { "FileType", "BufEnter" },
   {
     pattern = { "*.cs", "*.csharp" },
-    callback = set_options,
+    callback = set_and_run_cs_files,
+  })
+
+vim.api.nvim_create_autocmd(
+  { "FileType", "BufEnter" },
+  {
+    pattern = { "*.bash", "*.sh", "sh" },
+    callback = run_bash_files,
+  })
+
+vim.api.nvim_create_autocmd(
+  { "FileType", "BufEnter" },
+  {
+    pattern = { "*.lua" },
+    callback = run_lua_files,
+  })
+
+vim.api.nvim_create_autocmd(
+  { "FileType", "BufEnter" },
+  {
+    pattern = { "*.js" },
+    callback = run_js_files,
   })
